164e64aaae6d987a5ff230a0ee9702493bed0811
ismail harmanda <iharmanda@hotmail.com>
2021-07-07 20:57:25 +0300

    Links implemented inside routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <HashRouter>
+        <Route path="/" component={Header} />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" exact component={StreamCreate} />
         <Route path="/streams/edit" exact component={StreamEdit} />
