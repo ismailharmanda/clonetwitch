273c738e2d7a7e4dc91fe2c1b9d63bf2a24aecf6
ismail harmanda <iharmanda@hotmail.com>
2021-07-08 17:51:31 +0300

    Google Auth implemented and button added

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-class GoogleAuth extends React.Component {
-  componentDidMount() {
+function GoogleAuth(props) {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  const auth = useRef("");
+
+  useEffect(() => {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "100802994063-vohitce5mb1rf5mtkrhuc0nbgri957fk.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "564507930030-hkv35kvem2r4ra3bikh61mad7j3k9p85.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          auth.current = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.current.isSignedIn.get());
+          auth.current.isSignedIn.listen(onAuthChange);
+        });
     });
-  }
-  render() {
-    return <div>Google Auth</div>;
-  }
+  }, []);
+
+  const onAuthChange = () => {
+    setIsSignedIn(auth.current.isSignedIn.get());
+  };
+  const onSignIn = () => {
+    auth.current.signIn();
+  };
+
+  const onSignOut = () => {
+    auth.current.signOut();
+  };
+
+  const renderAuthButton = () => {
+    if (isSignedIn === null) {
+      return null;
+    } else if (isSignedIn) {
+      return (
+        <button onClick={onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  };
+
+  return <div>{renderAuthButton()}</div>;
 }
 
 export default GoogleAuth;
