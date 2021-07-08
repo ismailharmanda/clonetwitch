df60b0e4926f16be820bd80e1c9c45ba2ecb4d78
ismail harmanda <iharmanda@hotmail.com>
2021-07-08 21:01:11 +0300

    userId has been taken from Google Auth

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,13 @@
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null,
 };
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "SING_IN":
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payload };
     case "SIGN_OUT":
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
