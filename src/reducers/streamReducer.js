e6d97f504215ee4bf9734117ec3c2f94da6ea77a
ismail harmanda <iharmanda@hotmail.com>
2021-07-13 22:37:24 +0300

    Every action has been handled except "delete". lodash library has been installed, next step gonna implement delete action

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -5,3 +5,18 @@ import {
   EDIT_STREAM,
   DELETE_STREAM,
 } from "../actions/types";
+
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
