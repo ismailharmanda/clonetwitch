0f59074627bf74d9328f01a4cd0bfece0a1619f4
ismail harmanda <iharmanda@hotmail.com>
2021-07-15 17:29:26 +0300

    inital values has been set via redux form's special attribute

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmir = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={{ title: "EDIT ME", description: "Change me" }}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -18,4 +32,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
