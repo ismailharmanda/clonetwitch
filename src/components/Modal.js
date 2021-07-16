0dfa04f7021922521af0457d2af7046e3c4efad0
ismail harmanda <iharmanda@hotmail.com>
2021-07-16 17:24:06 +0300

    bubbling event prevented by adding e.stopPropogation() inner div.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,7 +8,10 @@ const Modal = (props) => {
       onClick={() => History.push("/")}
       className="ui dimmer modals visible active"
     >
-      <div className="ui standard modal visible active">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
         <div className="header">Delete Stream</div>
         <div className="content">
           <p>Are you sure you want to delete this stream?</p>
