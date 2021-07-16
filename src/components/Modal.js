31d5d6d95f3cd3beb0859a89a88180b5e430d6e1
ismail harmanda <iharmanda@hotmail.com>
2021-07-16 17:20:01 +0300

    Hiding modal while after clicking out of modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import History from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
+    <div
+      onClick={() => History.push("/")}
+      className="ui dimmer modals visible active"
+    >
       <div className="ui standard modal visible active">
-        mjadjkaljdlasdjsal asljd lasd
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          <p>Are you sure you want to delete this stream?</p>
+        </div>
+        <div className="actions">
+          <div class="ui primary button">Approve</div>
+          <div class="ui negative button">Cancel</div>
+        </div>
       </div>
     </div>,
     document.querySelector("#modal")
