7099b0b082af694a30a2f412abe8decd18a7a2bf
ismail harmanda <iharmanda@hotmail.com>
2021-07-16 17:41:24 +0300

    Make reusable, no more hardcoded modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,14 +12,11 @@ const Modal = (props) => {
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
+        <div className="header">{props.title}</div>
         <div className="content">
-          <p>Are you sure you want to delete this stream?</p>
+          <p>{props.content}</p>
         </div>
-        <div className="actions">
-          <div class="ui primary button">Approve</div>
-          <div class="ui negative button">Cancel</div>
-        </div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
