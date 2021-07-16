afb8beca88e32e2ff11667a8b64d1e04ec744a94
ismail harmanda <iharmanda@hotmail.com>
2021-07-16 18:12:43 +0300

    Publick directory is cleaner and also "<></>" integrated insted of React.Fragment

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -3,10 +3,10 @@ import Modal from "../Modal";
 
 const StreamDelete = () => {
   const actions = (
-    <React.Fragment>
+    <>
       <button className="ui button negative">Delete</button>
       <button className="ui button">Cancel</button>
-    </React.Fragment>
+    </>
   );
   return (
     <div>
